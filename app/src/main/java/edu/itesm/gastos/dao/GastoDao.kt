1985c73b2349462ebc23658b6109b1df4b924418
Jose Tlacuilo <tlacuilose@outlook.com>
2022-04-22 17:08:34 -0500

    Add room database.

--- app/src/main/java/edu/itesm/gastos/dao/GastoDao.kt
+++ app/src/main/java/edu/itesm/gastos/dao/GastoDao.kt
@@ -3,3 +3,15 @@ package edu.itesm.gastos.dao
 import androidx.room.*
 import edu.itesm.gastos.entities.Gasto
 
+@Dao
+interface GastoDao {
+    @Query("SELECT * FROM Gasto")
+    suspend fun getAllGastos(): List<Gasto>
+
+    @Query("SELECT SUM(monto) FROM Gasto")
+    suspend fun getTotalMontoGastos(): Double
+
+    @Insert
+    suspend fun insertGasto(gasto: Gasto)
+}
+
